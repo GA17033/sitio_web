235ca834c210c58c62453c81d7b685fb66ec1bac
alexisgarcia17 <alexis.garcia170698@gmail.com>
2023-02-17 13:55:20 -0600

    add cv

---
+++ src/Views/Test.vue
@@ -0,0 +1,19 @@
+
+<template>
+    
+</template>
+<script>
+import { jsPDF } from "jspdf";
+
+export default{
+    methods:{
+        generatePDF(){
+            const doc = new jsPDF();
+            doc.text("Hello world!", 10, 10);
+            doc.save("a4.pdf");
+        }
+    }
+}
+
+
+</script>
